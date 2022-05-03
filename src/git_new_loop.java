5bf3512c37a8159b93a3e713caf4786011f78ce2
devnextgen <nipa.patel2022@gmail.com>
2022-05-03 23:44:51 +0100

    newb

---
+++ src/git_new_loop.java
@@ -0,0 +1,12 @@
+public class git_new_loop {
+    public static void main(String args[])
+    {
+        int i = 1;
+        int n = 2;
+        while(i<=10)
+        {
+            System.out.println(n +" " +"*" +" " + i  + " = " +" "+(i * n));
+            i++;
+        }
+    }
+}
